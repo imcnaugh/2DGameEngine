03776b8e46c60014e042a58dbaa69fa493f1b0b1
Ian McNaughton <ianmcnaugh@gmail.com>
2014-02-09 02:19:56 -0500

    added java docs to entity class

--- Game/src/main/java/com/mcnaughty/domainObjects/Entity.java
+++ Game/src/main/java/com/mcnaughty/domainObjects/Entity.java
@@ -8,6 +8,12 @@ public abstract class Entity {
 	private double xLocation;
 	private double yLocation;
 	
+	/**
+	 * @param height Height of entity
+	 * @param width Width of entity
+	 * @param xStartLocation Starting X location of entity
+	 * @param yStartLocation Starting Y location of entity
+	 */
 	protected Entity(int height, int width, double xStartLocation, double yStartLocation){
 		this.height = height;
 		this.width = width;
@@ -15,20 +21,55 @@ public abstract class Entity {
 		this.yLocation = yStartLocation;
 	}
 	
+	/**
+	 * Moves the entity left a given number of units (probably pixels).</br>
+	 * If a negative is given the entity moves right.
+	 * 
+	 * @param xVal number of pixels to move left
+	 * @return current x location
+	 */
 	protected double moveLeft(double xVal){
 		return xLocation += xVal;
 	}
 	
+	/**
+	 * Moves the entity right a given number of units (probably pixels).</br>
+	 * If a negative is given the entity moves left.
+	 * 
+	 * @param xVal number of pixels to move right
+	 * @return current x location
+	 */
 	protected double moveRight(double xVal){
 		return xLocation -= xVal;
 	}
 	
+	/**
+	 * Moves the entity up a given number of units (probably pixels).</br>
+	 * If a negative is given the entity moves down.
+	 * 
+	 * @param yVal number of pixels to move up
+	 * @return current y location
+	 */
 	protected double moveUp(double yVal){
 		return yLocation += yVal;
 	}
 	
+	/**
+	 * Moves the entity down a given number of units (probably pixels).</br>
+	 * If a negative is given the entity moves up.
+	 * 
+	 * @param yVal number of pixels to move down
+	 * @return current y location
+	 */
 	protected double moveDown(double yVal){
 		return yLocation -= yVal;
 	}
+	
+	/**
+	 * called each tick
+	 * 
+	 * @param tick current tick
+	 */
+	protected abstract void onTick(long tick);
 
 }
