63d66e65f15e3dc22ca29c8bdc8ffca883e5def6
Ian McNaughton <ianmcnaugh@gmail.com>
2014-02-13 00:02:36 -0500

    added a name to the level class

--- Game/src/main/java/com/mcnaughty/gameCore/Level.java
+++ Game/src/main/java/com/mcnaughty/gameCore/Level.java
@@ -1,5 +1,19 @@
 package com.mcnaughty.gameCore;
 
 public abstract class Level {
+	
+	protected String name;
+
+	protected Level(String name){
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
