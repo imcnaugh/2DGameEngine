1e8d0bce04148d11d1747871ff1d1665bc6faad2
Ian McNaughton <ianmcnaugh@gmail.com>
2014-03-08 18:11:08 -0500

    ok i added a player object and gave it access to the input. But i think im at a point where i need to expand and add to the GUI to progress, unless maybe i can working on the game ticks... im not to sure

--- Game/src/main/java/com/mcnaughty/game/domainObjects/Player.java
+++ Game/src/main/java/com/mcnaughty/game/domainObjects/Player.java
@@ -1,6 +1,11 @@
 package com.mcnaughty.game.domainObjects;
 
+import com.mcnaughty.game.input.Input;
+
 public abstract class Player extends Entity {
+
+	// TODO auto wire this in in time
+	private Input input;
 
 	protected Player(int height, int width, double xStartLocation,
 			double yStartLocation) {
