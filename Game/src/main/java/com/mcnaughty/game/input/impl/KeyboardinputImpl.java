6c7fb1c71c0061108d0959c513e4f17bb7738dc8
Ian McNaughton <ianmcnaugh@gmail.com>
2014-03-08 17:27:56 -0500

    ok i think i have a working input class, at least for the 4 arrow keys, now i need to get a gui up to put this to the test

--- Game/src/main/java/com/mcnaughty/game/input/impl/KeyboardinputImpl.java
+++ Game/src/main/java/com/mcnaughty/game/input/impl/KeyboardinputImpl.java
@@ -1,15 +1,18 @@
 package com.mcnaughty.game.input.impl;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import com.mcnaughty.game.input.Input;
 
-public class KeyboardinputImpl implements Input{
-	
+public class KeyboardinputImpl implements Input, KeyListener {
+
 	private boolean upArrowPressed;
 	private boolean downArrowPressed;
 	private boolean leftArrowPressed;
 	private boolean rightArrowPressed;
-	
-	public KeyboardinputImpl(){
+
+	public KeyboardinputImpl() {
 		upArrowPressed = false;
 		downArrowPressed = false;
 		leftArrowPressed = false;
@@ -31,5 +34,49 @@ public class KeyboardinputImpl implements Input{
 	public boolean isRightArrowPressed() {
 		return rightArrowPressed;
 	}
+	
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()) {
+			case (KeyEvent.VK_KP_UP): {
+				upArrowPressed = true;
+				break;
+			}
+			case (KeyEvent.VK_KP_DOWN): {
+				downArrowPressed = true;
+				break;
+			}
+			case (KeyEvent.VK_KP_LEFT): {
+				leftArrowPressed = true;
+				break;
+			}
+			case (KeyEvent.VK_KP_RIGHT): {
+				rightArrowPressed = true;
+				break;
+			}
+		}
+	}
 
+	public void keyReleased(KeyEvent e) {
+		switch (e.getKeyCode()) {
+			case (KeyEvent.VK_KP_UP): {
+				upArrowPressed = false;
+				break;
+			}
+			case (KeyEvent.VK_KP_DOWN): {
+				downArrowPressed = false;
+				break;
+			}
+			case (KeyEvent.VK_KP_LEFT): {
+				leftArrowPressed = false;
+				break;
+			}
+			case (KeyEvent.VK_KP_RIGHT): {
+				rightArrowPressed = false;
+				break;
+			}
+		}
+	}
+
+	public void keyTyped(KeyEvent e) {
+	}
 }
