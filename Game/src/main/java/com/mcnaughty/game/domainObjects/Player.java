73d23abbc13f297e7134cc6d324ebde4736287ee
Ian McNaughton <ianmcnaugh@gmail.com>
2014-03-08 17:55:36 -0500

    added a player object

---
+++ Game/src/main/java/com/mcnaughty/game/domainObjects/Player.java
@@ -0,0 +1,9 @@
+package com.mcnaughty.game.domainObjects;
+
+public abstract class Player extends Entity {
+
+	protected Player(int height, int width, double xStartLocation,
+			double yStartLocation) {
+		super(height, width, xStartLocation, yStartLocation);
+	}
+}
