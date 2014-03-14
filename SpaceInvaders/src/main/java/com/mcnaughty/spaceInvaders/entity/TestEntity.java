0e6d5edbd3b43d9e74a6811c93a409935f1a241f
Ian McNaughton <IanMcNaugh@gmail.com>
2014-03-13 23:32:36 -0400

    im debating with myself, how do i add the image to the entity, but the class is here now so i need to figure it out

---
+++ SpaceInvaders/src/main/java/com/mcnaughty/spaceInvaders/entity/TestEntity.java
@@ -0,0 +1,20 @@
+package com.mcnaughty.spaceInvaders.entity;
+
+import java.awt.Image;
+
+import com.mcnaughty.game.domainObjects.Entity;
+
+public class TestEntity extends Entity {
+
+	protected TestEntity(int height, int width, double xStartLocation,
+			double yStartLocation, Image image) {
+		super(height, width, xStartLocation, yStartLocation, image);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void onTick(long tick) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
