c70fdd22d8dd67b423d09ccd8c72c52962b59364
Ian McNaughton <IanMcNaugh@gmail.com>
2014-03-13 22:51:31 -0400

    removed the testing beginning class, also added a gameplay class to handle ticks and entity movement

--- Game/src/main/java/com/mcnaughty/game/gameCore/GameCore.java
+++ Game/src/main/java/com/mcnaughty/game/gameCore/GameCore.java
@@ -1,37 +1,21 @@
 package com.mcnaughty.game.gameCore;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.mcnaughty.game.domainObjects.Entity;
 import com.mcnaughty.game.gui.MainGui;
 
 @Component
-public class GameCore implements Runnable {
-
-	private boolean gameRunning;
-	private long tickNumber = 0;
-
-	private Level[] levels;
+public class GameCore {
 
 	@Autowired
 	private MainGui maingui;
 
+	@Autowired
+	private GamePlay gamePlay;
+
 	public GameCore() {
+		Thread gamePlayThread = new Thread(gamePlay);
+		gamePlayThread.start();
 	}
-
-	public void run() {
-		gameRunning = true;
-
-		Level currentLevel = levels[0];
-
-		while (gameRunning) {
-
-			tickNumber++;
-		}
-
-	}
-
 }
---
+++ Game/src/main/java/com/mcnaughty/game/gameCore/GamePlay.java
@@ -0,0 +1,29 @@
+package com.mcnaughty.game.gameCore;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.mcnaughty.game.domainObjects.Entity;
+
+@Component
+public class GamePlay implements Runnable {
+
+	@Autowired
+	private EntityManager entityManager;
+
+	private boolean gameRunning;
+	private long tick;
+
+	public GamePlay() {
+		gameRunning = true;
+		tick = 0;
+	}
+
+	public void run() {
+		while (gameRunning) {
+			for (Entity entity : entityManager.getEntities()) {
+				entity.onTick(tick);
+			}
+		}
+	}
+}
