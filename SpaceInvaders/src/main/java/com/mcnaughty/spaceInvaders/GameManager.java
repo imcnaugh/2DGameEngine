1006904d0f416a314a27dd6742413c5ad26a2fe4
Ian McNaughton <IanMcNaugh@gmail.com>
2014-03-16 21:32:17 -0400

    ok i had to pull the player out into the applicaion context file, but that will work because all of this properties should be set in a properties file. the left right directions are switched, but that was kind of expected, but i can move an object around the screen using the arrow keys and it works!

--- SpaceInvaders/src/main/java/com/mcnaughty/spaceInvaders/GameManager.java
+++ SpaceInvaders/src/main/java/com/mcnaughty/spaceInvaders/GameManager.java
@@ -1,11 +1,8 @@
 package com.mcnaughty.spaceInvaders;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.mcnaughty.game.domainObjects.Entity;
 import com.mcnaughty.game.gameCore.EntityManager;
 import com.mcnaughty.game.gameCore.GameCore;
 import com.mcnaughty.spaceInvaders.entity.TestPlayer;
@@ -23,17 +20,13 @@ public class GameManager {
 	@Autowired
 	private EntityManager entityManager;
 
+	@Autowired
+	private TestPlayer player;
+
 	public void setupGame() {
-		try {
-			Entity player = new TestPlayer(100, 100, 100, 100,
-					"src/main/resources/images/test.jpg");
+		entityManager.getEntities().add(player);
 
-			entityManager.getEntities().add(player);
-
-			gameCore.startGame();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		gameCore.startGame();
 	}
 
 }
--- SpaceInvaders/src/main/java/com/mcnaughty/spaceInvaders/entity/TestPlayer.java
+++ SpaceInvaders/src/main/java/com/mcnaughty/spaceInvaders/entity/TestPlayer.java
@@ -8,15 +8,14 @@ import com.mcnaughty.game.input.impl.KeyboardinputImpl;
 public class TestPlayer extends Player {
 
 	// TODO dont hardcode this
-	private double speed = 30;
+	private double speed = 3;
 
 	public TestPlayer(int height, int width, double xStartLocation,
 			double yStartLocation, String imagePath) throws IOException {
 		super(height, width, xStartLocation, yStartLocation, imagePath);
-		// TODO this is not getting autowired because im using new to creat the
+		// TODO this is not getting autowired because im using new to create the
 		// object, but i think my bigger issue now is that im not refreshing the
 		// screen.
-		input = new KeyboardinputImpl();
 	}
 
 	@Override
@@ -26,7 +25,6 @@ public class TestPlayer extends Player {
 		 * pressed at the same time, I'm sure that could be a good thing but it
 		 * seems odd here. look at this later...
 		 */
-
 		if (input.isDownArrowPressed()) {
 			moveDown(speed);
 		}
