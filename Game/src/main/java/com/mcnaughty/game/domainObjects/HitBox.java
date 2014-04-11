279603b5ba73738849fd835b74389c83faa3cae1
Ian McNaughton <IanMcNaugh@gmail.com>
2014-04-10 22:05:48 -0400

    formatting

--- Game/src/main/java/com/mcnaughty/game/domainObjects/HitBox.java
+++ Game/src/main/java/com/mcnaughty/game/domainObjects/HitBox.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class HitBox {
 
 	/*
-	 * TODO somehow add the entity x and y cords to this,
-	 * or account for it somehow.
+	 * TODO somehow add the entity x and y cords to this, or account for it
+	 * somehow.
 	 * 
 	 * TODO also find a way to handle collisions
 	 */
@@ -14,14 +14,15 @@ public class HitBox {
 	private double rightX;
 	private double topY;
 	private double bottomY;
-	
+
 	public Enum type;
-	
+
 	private List<HitBox> nextedBoxes;
-	
+
 	private Entity rootEntity;
-	
-	public HitBox(double leftX, double rightX, double topY, double bottomY, Entity rootEntity){
+
+	public HitBox(double leftX, double rightX, double topY, double bottomY,
+			Entity rootEntity) {
 		this.leftX = leftX;
 		this.rightX = rightX;
 		this.topY = topY;
@@ -33,7 +34,8 @@ public class HitBox {
 		if ((otherBox.getLeftX() < this.leftX && otherBox.getLeftX() > this.rightX)
 				|| (otherBox.getRightX() < this.leftX && otherBox.getRightX() > this.rightX)) {
 			if ((otherBox.getTopY() < this.bottomY && otherBox.getTopY() > this.topY)
-					|| (otherBox.getBottomY() < this.bottomY && otherBox.getBottomY() > this.topY)) {
+					|| (otherBox.getBottomY() < this.bottomY && otherBox
+							.getBottomY() > this.topY)) {
 				return true;
 			}
 		}
